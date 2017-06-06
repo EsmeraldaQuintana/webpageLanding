7b072088f790003b6814f86eb279202b52b72cde
Esmeralda Quintana <quintaen195@potsdam.edu>
2017-06-06 16:30:10 -0400

    apparently ready to write code

---
+++ tasks/build.js
@@ -0,0 +1,17 @@
+
+const rollup = require('rollup');
+const buble = require('rollup-plugin-buble');
+
+
+
+
+// use rollup module, make src/main.js as entry point to code
+rollup.rollup({
+  entry: 'src/main.js',
+  plugins: [ buble() ]
+}).then(function(bundle){
+  bundle.write({
+    format: 'iife',
+    dest: 'dist/bundle.js'
+  });
+});

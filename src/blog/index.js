eca2f71d22b84e2a8ce0df828b7a5146df1cda75
Esmeralda Quintana <esmeralda@firedrivendevelopment.net>
2017-06-26 21:34:01 -0400

    add styling for all text

--- src/blog/index.js
+++ src/blog/index.js
@@ -3,8 +3,19 @@ import './blog.css';
 
 const htmlStr = `
     <div class="Blog">
-        <h1>Blog</h1>
-        <p>BLOG BLOG BLOG</p>
+        <h2>Blog</h2>
+        <p>Cras at blandit ligula, id viverra nibh. Ut convallis malesuada sem,
+        et pretium elit varius quis. Quisque risus orci, maximus non nunc vitae,
+         bibendum faucibus dolor. Donec eu odio orci. Duis quis scelerisque dolo
+         r, eu tempor tellus. Aliquam varius, eros a ullamcorper malesuada, feli
+         s diam suscipit enim, vitae condimentum elit libero ac risus. Suspendis
+         se neque est, ornare lacinia mollis gravida, sagittis et mi. Sed ullam
+         corper lacus nec ligula semper, nec vestibulum eros luctus. Etiam tempo
+         r enim malesuada, ultricies magna in, facilisis metus. Duis ante nisi,
+         hendrerit id magna nec, tempor dictum nisi. Quisque id egestas est, nec
+          condimentum felis. Nulla a varius nulla. Vestibulum cursus urna at tur
+          pis egestas, vel interdum lorem malesuada. Donec scelerisque tincidunt
+           pellentesque.</p>
     </div>`;
 
 let container;
